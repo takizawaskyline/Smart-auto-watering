7d39249ce99c7bfce766e736926d7e5d6afbf6b0
takizawaskyline <vorfolomeevnekoglaj@gmail.com>
2026-02-06 16:34:46 +0300

    Update Poliv.ino

--- Poliv.ino
+++ Poliv.ino
@@ -1,5 +1,11 @@
 #define GH_INCLUDE_PORTAL
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 bool flag = 0;
+bool mqttConnected = false;  // Добавлена переменная
+
 #define hum_sen1 15
 #define hum_sen2 2
 #define hum_sen3 4
@@ -25,49 +31,56 @@ int temp_sens_pin[7] = {13, 12, 14, 27, 26, 25, 33};
 #define ssid "CPOD"
 #define password "ApoX51s42wR7FDK8"
 
+const char* user_mqtt = "Poliv";
+const char* password_mqtt = "Iz8GUHz8";
+const int port_mqtt = 19778;
+const char* server_mqtt = "m9.wqtt.ru";
+const char* mqtt_topic_pub = "epstains_file/delo_na_ostrove/vecherinki/Andrey_Grygoriev";
+
 #include <Wire.h>
 #include <Drewduino_I2CRelay_PCA95x5.h>
-#include <Arduino.h>
-#include <GyverHub.h>
-#include <Pairs.h>
-#include <PairsExt.h>
-#include <PairsStatic.h>
-GyverHub hub;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
-  for(int i = 0; i > 6; i++) {
+  for(int i = 0; i < 6; i++) {
     pinMode(hum_sens_pin[i], INPUT);
   }
 
-  wifi();
-  hub.config(F("Poliv"), F("Poliv"), F(""));
-  hub.begin();
-  hub.onBuild(build);
+  client.setServer(server_mqtt, port_mqtt);
+  client.setBufferSize(256);  // Увеличиваем буфер сообщений
 
+  wifi();
 }
 
 void loop() {
-  hub.tick();
-  
 
-}
 
-void build(gh::Builder& b) {
-  if(b.beginRow()) {
-    b.Text_("ocenki", zahar);
-    b.widget.label("Данные с датчиков");
-    b.widget.rows(2);
+  if (!client.connected()) {
+    mqttConnected = false;
+    reconnectMQTT();
+  } else {
+    if (!mqttConnected) {
+      mqttConnected = true;
+      Serial.println("MQTT подключён стабильно!");
+    }
   }
-   b.endRow();
 
-  if (b.beginRow()) {
-   // b.Text_("ocenki", zahar);
-    b.widget.label("Тест peirs");
-    b.widget.rows(2);
-  }   
-  b.endRow();
+  client.loop();
+  char msg[10];
+  int a = 123;
+    snprintf(msg, 10, "%d", a);
+  /*%d добавляем сюда c  Символ
+    s  Символьная строка
+    d, i  Целое десятичное число
+    u  Целое без знаковое десятичное число
+    o  Целое восьмеричное число
+    x   Целое шестнадцатеричное число
+    */
+  client.publish(mqtt_topic_pub, msg);
+
 }
-
 
 void wifi() {
   delay(2000);
@@ -82,4 +95,28 @@ void wifi() {
   }
   Serial.println("Connected");
   Serial.println(WiFi.localIP());
+}
+
+
+void reconnectMQTT() {
+  static unsigned long lastAttempt = 0;
+  
+  // Пытаемся переподключаться не чаще 1 раза в 5 секунд
+  if (millis() - lastAttempt < 5000) {
+    return;
+  }
+  lastAttempt = millis();
+  
+  Serial.print("Попытка подключения к MQTT...");
+  
+  // Случайный ID клиента для избежания конфликтов
+  String clientId = "ESP32-" + String(random(0xFFFF), HEX);
+  
+  if (client.connect(clientId.c_str(), user_mqtt, password_mqtt)) {
+    Serial.println("Успешно!");
+    //client.subscribe(mqtt_topic_sub);
+  } else {
+    Serial.print("Ошибка, rc=");
+    Serial.println(client.state());
+  }
 }
